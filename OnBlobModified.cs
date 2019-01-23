93d194cbca80744735d5d29d5ee1dbfe46090d9a
Yadel Lopez <yopez83@gmail.com>
2019-01-23 02:12:40 -0500

    Add message to queue upon blob modified

--- OnBlobModified.cs
+++ OnBlobModified.cs
@@ -1,7 +1,10 @@
+using System;
 using System.IO;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Newtonsoft.Json;
 
 namespace AzurePractices.Function
 {
@@ -11,6 +14,32 @@ namespace AzurePractices.Function
         public static void Run([BlobTrigger("blob-container-example/{name}", Connection = "AzureWebJobsStorage")]Stream myBlob, string name, ILogger log)
         {
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+
+            dynamic messageDTO = new 
+            {
+                fromEmail = "yopez83@hotmail.com",
+                toEmail = "e3105548@miamidade.gov",
+                subject = "Blob Modified",
+                message = $"A blob has been modified.\n Name: {name}\n Size: {myBlob.Length} Bytes.",
+                isImportant = true
+            };
+
+            var jsonMessageDTO = JsonConvert.SerializeObject(messageDTO);
+
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable("AzureWebJobsStorage"));
+
+            // Create the queue client.
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+            // Retrieve a reference to a queue.
+            CloudQueue queue = queueClient.GetQueueReference("myqueue-items");
+
+            // Create the queue if it doesn't already exist.
+            queue.CreateIfNotExistsAsync();
+
+            // Create a message and add it to the queue.
+            CloudQueueMessage message = new CloudQueueMessage(jsonMessageDTO);
+            queue.AddMessageAsync(message);
         }
     }
 }
